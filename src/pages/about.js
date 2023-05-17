941c1caa547f32912cc3e0e067a8e3ef36bb1f53
lapislazuli21 <120363643+lapislazuli21@users.noreply.github.com>
2023-05-17 11:12:33 +0530

    Update about.js

--- src/pages/about.js
+++ src/pages/about.js
@@ -48,13 +48,13 @@ const about = () => {
                 <div className='grid w-full grid-cols-9 gap-16 sm:gap-8 dark:text-light'>
                     <div className='md:order-2 md:col-span-9 col-span-3 xl:col-span-4 flex flex-col items-start md:items-center justify-center '>
                         <h2 className='mb-4 text-xl font-bold text-dark/90 dark:text-light/90 capitalize'>A look behind the screen</h2>
-                        <p className='font-medium'>Hi, I'm CodeBucks, a web developer and UI/UX designer with a passion for creating beautiful, functional, 
+                        <p className='font-medium'>Hi, I&apos;m CodeBucks, a web developer and UI/UX designer with a passion for creating beautiful, functional, 
 and user-centered digital experiences. With 4 years of experience in the field. I am always looking for 
-new and innovative ways to bring my clients' visions to life.
+new and innovative ways to bring my clients&apos; visions to life.
 </p>
-                        <p className='font-medium my-4'>I believe that design is about more than just making things look pretty, it's about solving problems and 
+                        <p className='font-medium my-4'>I believe that design is about more than just making things look pretty, it&apos;s about solving problems and 
 creating intuitive, enjoyable experiences for users. </p>
-                        <p className='font-medium'>Whether I'm working on a website, mobile app, or 
+                        <p className='font-medium'>Whether I&apos;m working on a website, mobile app, or 
 other digital product, I bring my commitment to design excellence and user-centered thinking to 
 every project I work on. I look forward to the opportunity to bring my skills and passion to your next project.</p>
                     </div>
