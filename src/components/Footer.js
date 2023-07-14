00f411c88213d72ea509937b3e07bbd0051b720d
lapislazuli21 <khush.gandhi24@outlook.com>
2023-07-14 06:33:47 -0700

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,7 +8,7 @@ const Footer = () => {
         <Layout className='py-8 flex items-center justify-between dark:text-light lg:flex-col lg:py-6'>
             <span>{new Date().getFullYear()} &copy; All Rights Reserved.</span>
             <div className='flex items-center lg:py-2'>
-                Forged with<span className='text-xl px-1 sm:px-0 sm:text-base'>&nbsp;&#x1F525;&nbsp;</span> by&nbsp; <Link href="/">Khush Gandhi</Link>
+                Forged with<span className='text-xl px-1 sm:px-0 sm:text-base'>&nbsp;&#x1F525;&nbsp;</span> by&nbsp; <Link href="/about">Khush Gandhi</Link>
             </div>
             <Link href="/" className='text-primary dark:text-secondary'>Nothing to see here folks</Link> 
         </Layout>
