1a52c5662a6332b56581aa05f7841e76cf7e2446
lapislazuli21 <khush.gandhi24@outlook.com>
2024-02-17 13:37:33 +0530

    Fixed Footer mobile view

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,13 +5,13 @@ import Link from 'next/link'
 const Footer = () => {
   return (
     <footer className='w-full border-t-2 border-solid border-dark dark:border-light font-medium text-lg sm:text-base'>
-        <Layout className='py-8 flex items-center justify-between dark:text-light lg:flex-col lg:py-6'>
-            <span>{new Date().getFullYear()} &copy; All Rights Reserved.</span>
-            <div className='flex items-center lg:py-2'>
-                Forged with<span className=''>&nbsp;Next.js,</span><span className='text-[#38BDF7]'>&nbsp;Tailwind</span> <span className=''>&nbsp;and Vercel&nbsp;</span>by&nbsp; <Link href="/about" className='text-primary dark:text-secondary'>Khush Gandhi</Link>
-            </div>
-            <Link href="/" className='text-primary dark:text-secondary'>Nothing to see here folks</Link> 
-        </Layout>
+      <Layout className='py-8 flex items-center justify-between dark:text-light lg:flex-col lg:py-6'>
+        <span>{new Date().getFullYear()} &copy; All Rights Reserved.</span>
+        <div className='flex items-center lg:py-2 sm:flex-col sm:'>
+          Forged with Next.js,<span className='text-[#38BDF7]'>&nbsp;Tailwind,&nbsp;</span>and Vercel.
+        </div>
+        <Link href="/about" className='text-primary dark:text-secondary'>Developed by Khush Gandhi</Link>
+      </Layout>
     </footer>
   )
 }
